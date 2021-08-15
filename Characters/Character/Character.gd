87ef2a4b1b520b9ab0535cf7382711ce7f2ddb69
YaroslavFox <yaroslavfox@inbox.ru>
2021-08-15 20:40:48 +0300

    Some character changes

--- Characters/Character/Character.gd
+++ Characters/Character/Character.gd
@@ -8,7 +8,7 @@ func _move():
 	_velocity = move_and_slide(Vector3(_controller.input_vector.x, 0.0, _controller.input_vector.y)*5)
 	
 func _rotate(_delta:float = 0.0):
-	rotation.y = lerp_angle(rotation.y, _controller.input_rotation, .125)
+	rotation.y = lerp_angle(rotation.y, _controller.input_rotation, .125/2)
 	
 func _physics_process(delta):
 	_move()
--- Globals/ControlStates.gd
+++ Globals/ControlStates.gd
@@ -7,12 +7,30 @@ enum TYPES{
 	TOUCHSCREEN
 }
 
+
+
 var type = TYPES.KEYBOARD_MOUSE
 
+onready var joy_id = Input.get_connected_joypads()[0] if Input.get_connected_joypads() else -1
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.connect("joy_connection_changed", self, "_joy_connection_changed")
+
+func _joy_connection_changed(device: int, connected: bool):
+	if connected:
+		joy_id = device
+	elif joy_id == device:
+		joy_id = -1
+		
+
 func _input(event):
-	if event is InputEventMouse or event is InputEventKey:
+	if event.is_action_pressed("to_menu"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	elif event is InputEventMouse or event is InputEventKey:
 		type = TYPES.KEYBOARD_MOUSE
-	elif event is InputEventJoypadButton:
+	elif typeof(event) in [typeof(InputEventJoypadButton), typeof(InputEventJoypadMotion)] :
 		type = TYPES.GAMEPAD
 	elif event is InputEventScreenTouch:
 		type = TYPES.TOUCHSCREEN
+	
