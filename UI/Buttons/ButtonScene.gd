7fca8fcead33a3dbe9da203dd610c3520ba59a2d
YaroslavFox <yaroslavfox@inbox.ru>
2021-08-25 20:29:16 +0300

    I's been fixed

--- UI/Buttons/ButtonScene.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-export(PackedScene) var scene_to_open
-
-
-func _ready() -> void:
-	if scene_to_open:
-		connect("pressed", get_tree(), "change_scene_to", [scene_to_open])
--- UI/MainMenu/MainMenu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-onready var animator := $Background/CameraAnimator
-
-func _start_button_focused() -> void:
-	animator.play("StartButtonCameraTransition")
-
-func _start_button_unfocused() -> void:
-	animator.play_backwards('StartButtonCameraTransition')
